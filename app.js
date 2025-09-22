a900e3e90078f39960aad27bb0f8cf4ff306d900
Rocio Garcia Cabrera <rociiio.gc@hotmail.com>
2025-09-22 22:35:50 +0100

    Seccion de favoritos

--- app.js
+++ app.js
@@ -3,3 +3,8 @@
 const COUNTRIES_URL = "https://restcountries.com/v3.1/all?fields=name,cca3,region,languages,flags,population,capital";
 const MOCKAPI_URL   = "https://68bdeb02227c48698f85a6c4.mockapi.io/api/v1/favorites";
 
+let COUNTRIES = [];
+let FILTERED  = [];
+let FAVORITES = [];
+
+let editingFavId = null;
