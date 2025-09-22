c8caf31eecbe1a8b86a7566ecee2f8f18ad5f966
Rocio Garcia Cabrera <rociiio.gc@hotmail.com>
2025-09-22 21:56:20 +0100

    Primer commit - subir proyecto Países del Mundo + config inicial

---
+++ app.js
@@ -0,0 +1,5 @@
+// Configuracion inicial
+// =======================
+const COUNTRIES_URL = "https://restcountries.com/v3.1/all?fields=name,cca3,region,languages,flags,population,capital";
+const MOCKAPI_URL   = "https://68bdeb02227c48698f85a6c4.mockapi.io/api/v1/favorites";
+
